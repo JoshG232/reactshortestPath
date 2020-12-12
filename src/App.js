1a472080f0f611a37aad6d9ce0fd928e03ecdc05
JoshG232 <62026593+JoshG232@users.noreply.github.com>
2020-12-12 13:24:38 +0000

    first ting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {useState} from "react";
+
+import Tweet from "./components/tweat"
 function App() {
+
+  const [users] = useState([
+    { name: "Josh", message:"Damm"},
+    { name: "Brick", message:"Nice"}
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map(user => (
+        <Tweet name={user.name} message = {user.message} />
+      ))}
+
+
     </div>
   );
 }
